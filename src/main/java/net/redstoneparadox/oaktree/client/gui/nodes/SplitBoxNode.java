0b98b74f5e58d913510cee8bc10cc0db35d451a0
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-25 15:15:42 -0700

    Added Javadox to SplitBoxNode

--- src/main/java/net/redstoneparadox/oaktree/client/gui/nodes/SplitBoxNode.java
+++ src/main/java/net/redstoneparadox/oaktree/client/gui/nodes/SplitBoxNode.java
@@ -3,10 +3,23 @@ package net.redstoneparadox.oaktree.client.gui.nodes;
 import net.redstoneparadox.oaktree.client.gui.OakTreeGUI;
 import net.redstoneparadox.oaktree.client.gui.util.NodeDirection;
 
+/**
+ * A SplitBoxNode encapsulates two {@link BoxNode} instances,
+ * maintaining a percentage-based split between them. The
+ * width or height of the first box takes up the specified
+ * percentage of the total width while the second box takes
+ * up the remainder.
+ *
+ * The {@link BoxNode#setMargin(float)},
+ * {@link BoxNode#setMargin(NodeDirection, float)}, and
+ * {@link BoxNode#setChild(Node)} are encapsulated by the
+ * SplitBoxNode as well, so there is no need to access
+ * them directly.
+ */
 public class SplitBoxNode extends Node<SplitBoxNode> {
 
-    public final BoxNode first = new BoxNode();
-    public final BoxNode second = new BoxNode();
+    private final BoxNode first = new BoxNode();
+    private final BoxNode second = new BoxNode();
 
     public float splitPercent = 50.0f;
 
@@ -17,6 +30,13 @@ public class SplitBoxNode extends Node<SplitBoxNode> {
         second.expand = true;
     }
 
+    /**
+     * Sets the percentage of the split. A split of 50%, for example,
+     * will mean that both sides take up half of the space.
+     *
+     * @param percent The percent of the split. Values are in pixels.
+     * @return The node itself.
+     */
     public SplitBoxNode setSplitPercent(float percent) {
         if (percent <= 0.0f) {
             percent = 0.0f;
@@ -28,36 +48,93 @@ public class SplitBoxNode extends Node<SplitBoxNode> {
         return this;
     }
 
-    public SplitBoxNode setVertical(boolean value) {
-        vertical = value;
+    /**
+     * Sets whether or not this node is oriented vertically. If
+     * oriented vertically, the split will be horizontal.
+     *
+     * @param vertical If the SplitBoxContainer should be
+     *                 oriented vertically.
+     * @return The node itself.
+     */
+    public SplitBoxNode setVertical(boolean vertical) {
+        this.vertical = vertical;
         return this;
     }
 
+    /**
+     * Sets the first child of this node. The first child
+     * will be drawn on the left side of the node or the
+     * top side if the node is oriented vertically.
+     *
+     * @param child The first child.
+     * @return The node itself.
+     */
     public SplitBoxNode setFirstChild(Node child) {
         first.setChild(child);
         return this;
     }
 
+    /**
+     * Sets the second child of this node. The second
+     * child will be drawn on the right side of the
+     * node or the top side if the node is oriented
+     * vertically.
+     *
+     * @param child The second child.
+     * @return The node itself.
+     */
     public SplitBoxNode setSecondChild(Node child) {
         second.setChild(child);
         return this;
     }
 
-    public SplitBoxNode setFirstMargin(float value) {
-        first.setMargin(value);
+    /**
+     * Sets all 4 internal margins of the first box
+     * (left or top).
+     *
+     * @param margin The size of the margin in pixels.
+     * @return The node itself
+     */
+    public SplitBoxNode setFirstMargin(float margin) {
+        first.setMargin(margin);
         return this;
     }
 
+    /**
+     * Sets one of the 4 internal margins of the first
+     * box based on the passed direction.
+     *
+     * @param direction The direction of the corresponding
+     *                  margin.
+     * @param margin The size of the margin in pixels.
+     * @return The node itself.
+     */
     public SplitBoxNode setFirstMargin(NodeDirection direction, float margin) {
         first.setMargin(direction, margin);
         return this;
     }
 
-    public SplitBoxNode setSecondMargin(float value) {
-        second.setMargin(value);
+    /**
+     * Sets all 4 internal margins of the second box
+     * (left or top).
+     *
+     * @param margin The size of the margin in pixels.
+     * @return The node itself.
+     */
+    public SplitBoxNode setSecondMargin(float margin) {
+        second.setMargin(margin);
         return this;
     }
 
+    /**
+     * Sets one of the 4 internal margins of the second
+     * box based on the passed direction.
+     *
+     * @param direction The direction of the corresponding
+     *                  margin.
+     * @param margin The size of the margin in pixels.
+     * @return The node itself.
+     */
     public SplitBoxNode setSecondMargin(NodeDirection direction, float margin) {
         second.setMargin(direction, margin);
         return this;
