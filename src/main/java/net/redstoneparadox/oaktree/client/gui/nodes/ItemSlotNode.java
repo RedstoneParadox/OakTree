561d3d6cfd74b40b0d672b962c5d9f5ffd8b9a10
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-26 15:28:57 -0700

    Added javadoc to ItemSlotNode

--- src/main/java/net/redstoneparadox/oaktree/client/gui/nodes/ItemSlotNode.java
+++ src/main/java/net/redstoneparadox/oaktree/client/gui/nodes/ItemSlotNode.java
@@ -1,5 +1,6 @@
 package net.redstoneparadox.oaktree.client.gui.nodes;
 
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.container.Slot;
 import net.redstoneparadox.oaktree.client.gui.OakTreeGUI;
 import net.redstoneparadox.oaktree.client.networking.OakTreeClientNetworking;
@@ -7,11 +8,19 @@ import net.redstoneparadox.oaktree.client.networking.OakTreeClientNetworking;
 /**
  * A Node that acts as a special wrapper for setting
  * the position of an item slot.
+ *
+ * If attached to a {@link Screen} that does not
+ * have a container, it will not be drawn.
  */
 public class ItemSlotNode extends Node<ItemSlotNode> {
 
     private final int index;
 
+    /**
+     * @param index The index of the slot in your
+     *              container's slot list, not the
+     *              index value of the slot.
+     */
     public ItemSlotNode(int index) {
         this.index = index;
     }
