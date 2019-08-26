f6f67e63667f7c21eb069b0a4d2bcb05ed439e2c
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-25 18:44:13 -0700

    Tests are now only run in dev with the dedicated server

--- src/main/java/net/redstoneparadox/oaktree/OakTree.java
+++ src/main/java/net/redstoneparadox/oaktree/OakTree.java
@@ -1,13 +1,17 @@
 package net.redstoneparadox.oaktree;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.redstoneparadox.oaktree.networking.Packets;
 import net.redstoneparadox.oaktree.test.Tests;
 
 public class OakTree implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		System.out.println("Loading Oak Tree GUI Toolkit...");
-
-		new Tests().init();
+		if (FabricLoader.getInstance().isDevelopmentEnvironment() && FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			new Tests().init();
+		}
 	}
 }
