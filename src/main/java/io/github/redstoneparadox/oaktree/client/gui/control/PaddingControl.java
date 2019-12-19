524238e7542bfab227ce3004f42aad57f8b2fc78
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-18 20:41:34 -0800

    Created PanelControl

--- src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/PaddingControl.java
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/PaddingControl.java
@@ -1,10 +1,17 @@
 package io.github.redstoneparadox.oaktree.client.gui.control;
 
+import io.github.redstoneparadox.oaktree.client.gui.OakTreeGUI;
+
 public abstract class PaddingControl<C extends Control> extends Control<C> {
-    float topPadding = 0.0f;
-    float bottomPadding = 0.0f;
-    float leftPadding = 0.0f;
-    float rightPadding = 0.0f;
+    public float topPadding = 0.0f;
+    public float bottomPadding = 0.0f;
+    public float leftPadding = 0.0f;
+    public float rightPadding = 0.0f;
+
+    float innerWidth = width;
+    float innerHeight = height;
+    float innerX = trueX;
+    float innerY = trueY;
 
     /**
      * Sets the padding for all 4 sides.
@@ -62,5 +69,15 @@ public abstract class PaddingControl<C extends Control> extends Control<C> {
     public C rightPadding(float rightPadding) {
         this.rightPadding = rightPadding;
         return (C) this;
+    }
+
+    @Override
+    public void preDraw(int mouseX, int mouseY, float deltaTime, OakTreeGUI gui, float offsetX, float offsetY, float containerWidth, float containerHeight) {
+        super.preDraw(mouseX, mouseY, deltaTime, gui, offsetX, offsetY, containerWidth, containerHeight);
+        innerWidth = width - leftPadding - rightPadding;
+        innerHeight = height - topPadding - bottomPadding;
+
+        innerX = trueX + leftPadding;
+        innerY = trueY + topPadding;
     }
 }
---
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/PanelControl.java
@@ -0,0 +1,37 @@
+package io.github.redstoneparadox.oaktree.client.gui.control;
+
+import io.github.redstoneparadox.oaktree.client.gui.OakTreeGUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A {@link Control} containing any number of children
+ * {@link Control} instances. Children are drawn
+ * without any arrangement applied to them; subclasses
+ * offer various arrangement options.
+ *
+ * @param <C> The {@link PanelControl} type.
+ */
+public class PanelControl<C extends PanelControl> extends PaddingControl<C> {
+
+    public final List<Control> children = new ArrayList<>();
+
+    public C child(Control child) {
+        children.add(child);
+        return (C) this;
+    }
+
+    @Override
+    public void preDraw(int mouseX, int mouseY, float deltaTime, OakTreeGUI gui, float offsetX, float offsetY, float containerWidth, float containerHeight) {
+        if (!visible) return;
+        super.preDraw(mouseX, mouseY, deltaTime, gui, offsetX, offsetY, containerWidth, containerHeight);
+        preDrawChildren(mouseX, mouseY, deltaTime, gui);
+    }
+
+    void preDrawChildren(int mouseX, int mouseY, float deltaTime, OakTreeGUI gui) {
+        for (Control child: children) {
+            child.preDraw(mouseX, mouseY, deltaTime, gui, innerX, innerY, innerWidth, innerHeight);
+        }
+    }
+}
