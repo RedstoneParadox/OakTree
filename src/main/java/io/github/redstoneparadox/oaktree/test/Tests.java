cca7e65d59ea3eb583f408af3a3bcf7ec0752a2e
RedstoneParadox <spinjitzu00@gmail.com>
2020-02-04 15:47:13 -0800

    Added new test to verify recently added nodes

--- src/main/java/io/github/redstoneparadox/oaktree/test/Tests.java
+++ src/main/java/io/github/redstoneparadox/oaktree/test/Tests.java
@@ -1,9 +1,9 @@
 package io.github.redstoneparadox.oaktree.test;
 
 import io.github.redstoneparadox.oaktree.client.gui.ScreenBuilder;
-import io.github.redstoneparadox.oaktree.client.gui.control.Control;
-import io.github.redstoneparadox.oaktree.client.gui.control.TextEditControl;
+import io.github.redstoneparadox.oaktree.client.gui.control.*;
 import io.github.redstoneparadox.oaktree.client.gui.style.ColorStyleBox;
+import io.github.redstoneparadox.oaktree.client.gui.util.ControlAnchor;
 import io.github.redstoneparadox.oaktree.client.gui.util.RGBAColor;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Block;
@@ -24,9 +24,11 @@ import java.util.function.Supplier;
 
 public class Tests {
     private final TestBlock testOne = new TestBlock(testSettings(), this::testOne);
+    private final TestBlock testTwo = new TestBlock(testSettings(), this::testTwo);
 
     public void init() {
         register(testOne, "one");
+        register(testTwo, "two");
     }
 
     private Block.Settings testSettings() {
@@ -37,10 +39,10 @@ public class Tests {
         Registry.register(Registry.BLOCK, new Identifier("oaktree", "test_" + testNum), block);
     }
 
-    class TestBlock extends Block {
+    static class TestBlock extends Block {
         private final Supplier<Control> supplier;
 
-        public TestBlock(Settings settings, Supplier<Control> supplier) {
+        TestBlock(Settings settings, Supplier<Control> supplier) {
             super(settings);
             this.supplier = supplier;
         }
@@ -60,5 +62,53 @@ public class Tests {
                 .defaultStyle(new ColorStyleBox(RGBAColor.black()))
                 .maxLines(20)
                 .displayedLines(10);
+    }
+
+    private Control testTwo() {
+        PagePanelControl pagePanelControl = new PagePanelControl();
+
+        return new SplitPanelControl()
+                .size(200, 300)
+                .splitSize(200)
+                .verticalSplit(true)
+                .child(
+                        pagePanelControl
+                                .size(200, 200)
+                                .defaultStyle(new ColorStyleBox(RGBAColor.black()))
+                                .child(
+                                        new LabelControl()
+                                                .size(200, 50)
+                                                .text("Page 1.")
+                                )
+                                .child(
+                                        new LabelControl()
+                                                .size(200, 50)
+                                                .text("Page 2.")
+                                )
+                                .child(
+                                        new LabelControl()
+                                                .size(200, 50)
+                                                .text("Page 3.")
+                                )
+                )
+                .child(
+                        new SplitPanelControl()
+                                .size(200, 100)
+                                .splitSize(100)
+                                .child(
+                                new ButtonControl()
+                                .size(50, 50)
+                                .anchor(ControlAnchor.CENTER)
+                                .defaultStyle(new ColorStyleBox(RGBAColor.red()))
+                                .onClick((gui, control) -> pagePanelControl.previousPage())
+                                )
+                                .child(
+                                        new ButtonControl()
+                                                .size(50, 50)
+                                                .anchor(ControlAnchor.CENTER)
+                                                .defaultStyle(new ColorStyleBox(RGBAColor.green()))
+                                                .onClick((gui, control) -> pagePanelControl.nextPage())
+                                )
+                );
     }
 }
