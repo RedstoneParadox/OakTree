5717b289b73ddc814120568a4bbcf4ae869c759c
RedstoneParadox <spinjitzu00@gmail.com>
2020-02-04 15:15:40 -0800

    Create PagePanelControl.java

---
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/PagePanelControl.java
@@ -0,0 +1,41 @@
+package io.github.redstoneparadox.oaktree.client.gui.control;
+
+import io.github.redstoneparadox.oaktree.client.gui.OakTreeGUI;
+
+public class PagePanelControl extends PanelControl<PagePanelControl> {
+    public int page = 0;
+
+    public PagePanelControl page(int page) {
+        if (page < 0) this.page = 0;
+        else if (page >= children.size()) this.page = children.size() - 1;
+        else this.page = page;
+
+        return this;
+    }
+
+    public PagePanelControl nextPage() {
+        page(page + 1);
+        return this;
+    }
+
+    public PagePanelControl previousPage() {
+        page(page - 1);
+        return this;
+    }
+
+    public PagePanelControl flipPages(int count) {
+        page(page + count);
+        return this;
+    }
+
+    @Override
+    void arrangeChildren(int mouseX, int mouseY, float deltaTime, OakTreeGUI gui) {
+        Control child = children.get(page);
+        if (child != null) child.preDraw(mouseX, mouseY, deltaTime, gui, innerX, innerY, innerWidth, innerHeight);
+    }
+
+    @Override
+    boolean shouldDraw(Control child) {
+        return children.indexOf(child) == page;
+    }
+}
