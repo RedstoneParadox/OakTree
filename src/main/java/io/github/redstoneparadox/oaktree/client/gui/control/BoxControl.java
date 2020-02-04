ff2c722686171384286fb3ff0cf3c2a52c7d999c
RedstoneParadox <spinjitzu00@gmail.com>
2020-02-04 14:56:06 -0800

    Reworked BoxControl so that it is now a child of PaddingControl

--- src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/BoxControl.java
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/BoxControl.java
@@ -4,47 +4,17 @@ import io.github.redstoneparadox.oaktree.client.gui.OakTreeGUI;
 import io.github.redstoneparadox.oaktree.client.gui.util.ControlDirection;
 
 /**
- * BoxNode is a type of Node that can have a single child
- * node and internal draw margins.
+ * BoxControl is a type of PaddingControl that
+ * can have a single child node.
+ *
+ * While {@link PanelControl} could also be used,
+ * BoxControl is specialized for dealing with a
+ * single child.
  */
-public class BoxControl extends Control<BoxControl> {
-
-    public float topPadding = 0.0f;
-    public float bottomPadding = 0.0f;
-    public float leftPadding = 0.0f;
-    public float rightPadding = 0.0f;
-
+public class BoxControl extends PaddingControl<BoxControl> {
     public Control child = null;
 
-    /**
-     * Sets all 4 internal margins for this BoxNode.
-     *
-     * @param margin The new margin.
-     * @return The node itself.
-     */
-    public BoxControl padding(float margin) {
-        if (margin <= 0.0f) {
-            margin = 0.0f;
-        }
-
-        topPadding = margin;
-        bottomPadding = margin;
-        leftPadding = margin;
-        rightPadding = margin;
-        return this;
-    }
-
-    /**
-     * Sets one of the four margins based on the passed direction.
-     * A direction value is passed so that nodes such as
-     * {@link SplitBoxControl} which encapsulate multiple BoxNodes
-     * don't have confusing/verbose method names such as
-     * `setLeftMarginOfFirst`.
-     *
-     * @param direction Direction representing the margin to set.
-     * @param margin The size of the margin. Values are in pixels.
-     * @return the node itself.
-     */
+    @Deprecated
     public BoxControl padding(ControlDirection direction, float margin) {
         switch (direction) {
             case UP:
