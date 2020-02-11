d22f76eb677d431d5c6fe7cf9192b6e316ff9ec5
RedstoneParadox <spinjitzu00@gmail.com>
2020-02-11 14:22:33 -0800

    Changed TextureStyleBox drawing so that it could be used for tiling in NinePatchStyleBox

--- src/main/java/io/github/redstoneparadox/oaktree/client/gui/style/TextureStyleBox.java
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/style/TextureStyleBox.java
@@ -9,16 +9,19 @@ import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.util.Identifier;
 import io.github.redstoneparadox.oaktree.client.gui.OakTreeGUI;
+import org.lwjgl.opengl.GL11;
 
 public class TextureStyleBox extends StyleBox {
     Identifier textureID;
     int drawLeft = 0;
     int drawTop = 0;
-    int magicNumber = 1;
     private boolean tiled;
     private int textureWidth = 0;
     private int textureHeight = 0;
     private RGBAColor tint = RGBAColor.white();
+    private int fileWidth = 0;
+    private int fileHeight = 0;
+    private int scale = 2;
 
     public TextureStyleBox(String path) {
         textureID = new Identifier(path);
@@ -31,31 +34,8 @@ public class TextureStyleBox extends StyleBox {
     }
 
     /**
-     * <p>
-     * Sets the magic number to multiply the left, top,
-     * width, and height by when drawing. You may need
-     * to adjust this depending on the side of the
-     * texture file you are using.
-     * </p>
-     *
-     * <p>
-     * For example, if the file dimensions are 16x16,
-     * the magic number should be 2.
-     * </p>
-     *
-     * @param magicNumber The value to set.
-     * @return The stylebox itself.
-     */
-    public TextureStyleBox magicNumber(int magicNumber) {
-        this.magicNumber = magicNumber;
-        return this;
-    }
-
-    /**
      * Sets whether or not the StyleBox
-     * should tile its texture; it's
-     * recommended that you have a
-     * separate texture file for this.
+     * should tile its texture.
      *
      * @param tiled The value to set.
      * @return The StyleBox itself.
@@ -71,8 +51,26 @@ public class TextureStyleBox extends StyleBox {
         return this;
     }
 
+    public TextureStyleBox fileDimensions(int fileWidth, int fileHeight) {
+        this.fileWidth = fileWidth;
+        this.fileHeight = fileHeight;
+        return this;
+    }
+
     public TextureStyleBox tint(RGBAColor tint) {
         this.tint = tint;
+        return this;
+    }
+
+    /**
+     * If texture dimensions are wonky, you
+     * may need to change the scale.
+     *
+     * @param scale The value to set.
+     * @return This
+     */
+    public TextureStyleBox scale(int scale) {
+        this.scale = scale;
         return this;
     }
 
@@ -84,18 +82,36 @@ public class TextureStyleBox extends StyleBox {
                 int drawWidth = Math.min((int)width, textureWidth);
                 int drawHeight = Math.min((int)height, textureHeight);
 
-                drawTexture(x, y, drawLeft * magicNumber, drawTop * magicNumber, drawWidth * magicNumber, drawHeight * magicNumber);
+                drawTexture(x, y, drawLeft, drawTop, drawWidth, drawHeight);
             }
             else {
-                drawTexture(x, y, drawLeft * magicNumber, drawTop * magicNumber, width * magicNumber, height * magicNumber);
+                drawTiled(x, y, drawLeft, drawTop, textureWidth, textureHeight, (int)width, (int)height);
+            }
+        }
+    }
+
+    void drawTiled(float x, float y, int left, int top, int drawWidth, int drawHeight, int width, int height) {
+        int remainingWidth = width;
+        int remainingHeight = height;
+
+        while (remainingHeight > 0) {
+            float currentX = x + (width - remainingWidth);
+            float currentY = y + (height - remainingHeight);
+
+            float minWidth = Math.min(remainingWidth, drawWidth);
+            float minHeight = Math.min(remainingHeight, drawHeight);
+
+            drawTexture(currentX, currentY, left, top, minWidth, minHeight);
+
+            remainingWidth -= drawWidth;
+            if (remainingWidth < 0) {
+                remainingWidth = width;
+                remainingHeight -= drawHeight;
             }
         }
     }
 
     private void drawTexture(float x, float y, float left, float top, float width, float height) {
-        float widthDivisor = textureWidth * 2.0f;
-        float heightDivisor = textureHeight * 2.0f;
-
         int r = (int) (tint.redChannel * 255.0f);
         int g = (int) (tint.greenChannel * 255.0f);
         int b = (int) (tint.blueChannel * 255.0f);
@@ -103,12 +119,20 @@ public class TextureStyleBox extends StyleBox {
 
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuffer();
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        bufferBuilder.begin(7, VertexFormats.POSITION_TEXTURE_COLOR);
-        bufferBuilder.vertex(x, (y + height), 0.0).texture(left/widthDivisor, (height + top)/heightDivisor).color(r, g, b, a).next();
-        bufferBuilder.vertex((x + width), (y + height), 0.0).texture((width + left)/widthDivisor, (height + top)/heightDivisor).color(r, g, b, a).next();
-        bufferBuilder.vertex((x + width), y, 0.0).texture((width + left)/widthDivisor, top/heightDivisor).color(r, g, b, a).next();
-        bufferBuilder.vertex(x, y, 0.0).texture(left/widthDivisor, top/heightDivisor).color(r, g, b, a).next();
+
+        RenderSystem.enableBlend();
+        RenderSystem.blendFuncSeparate(770, 771, 1, 0);
+        RenderSystem.color4f(255, 255, 255, 255);
+
+        bufferBuilder.begin(GL11.GL_QUADS, VertexFormats.POSITION_TEXTURE);
+
+        bufferBuilder.vertex(x * scale, (y + height) * scale, 0.0).texture(left/fileWidth, (top + textureHeight)/fileHeight).color(r, g, b, a).next();
+        bufferBuilder.vertex((x + width) * scale, (y + height) * scale, 0.0).texture((left + textureWidth)/fileWidth, (top + textureHeight)/fileHeight).color(r, g, b, a).next();
+        bufferBuilder.vertex((x + width) * scale, y * scale, 0.0).texture((left + textureWidth)/fileWidth, top/fileHeight).color(r, g, b, a).next();
+        bufferBuilder.vertex(x * scale, y * scale, 0.0).texture(left/fileWidth, top/fileHeight).color(r, g, b, a).next();
+
         tessellator.draw();
+
+        RenderSystem.disableBlend();
     }
 }
