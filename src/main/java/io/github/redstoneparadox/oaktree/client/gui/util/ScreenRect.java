39b885f1c06972e70086918b5c68a77303b769bf
RedstoneParadox <spinjitzu00@gmail.com>
2020-05-27 16:34:40 -0700

    Create ScreenRect.java

---
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/util/ScreenRect.java
@@ -0,0 +1,15 @@
+package io.github.redstoneparadox.oaktree.client.gui.util;
+
+public class ScreenRect {
+    float x;
+    float y;
+    float width;
+    float height;
+
+    public ScreenRect(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
