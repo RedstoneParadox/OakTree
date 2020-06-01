bde364a308e2053435e16e182b615be6963ce7f3
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-01 14:29:27 -0700

    Marked internal method as internal

--- src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/InteractiveControl.java
+++ src/main/java/io/github/redstoneparadox/oaktree/client/gui/control/InteractiveControl.java
@@ -1,20 +1,28 @@
 package io.github.redstoneparadox.oaktree.client.gui.control;
 
+import io.github.redstoneparadox.oaktree.client.gui.ControlGui;
 import io.github.redstoneparadox.oaktree.client.gui.util.ScreenVec;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class InteractiveControl<C extends InteractiveControl<C>> extends Control<C> {
+    public @Nullable Control<?> tooltip = null;
+    public boolean lockTooltipPos = false;
 
     protected boolean isMouseWithin = false;
+
+    @Override
+    public void preDraw(ControlGui gui, int offsetX, int offsetY, int containerWidth, int containerHeight, int mouseX, int mouseY) {
+        super.preDraw(gui, offsetX, offsetY, containerWidth, containerHeight, mouseX, mouseY);
+
+
+    }
 
     protected ScreenVec relativeMousePosition(int mouseX, int mouseY) {
         return new ScreenVec(mouseX - trueX, mouseY - trueY);
     }
-
-    /**
-     * INTERNAL API!!! DO NOT CALL!!!
-     * @param isMouseWithin [DATA EXPUNGED]
-     */
-    @Deprecated
+    
+    @ApiStatus.Internal
     public void setMouseWithin(boolean isMouseWithin) {
         this.isMouseWithin = isMouseWithin;
     }
