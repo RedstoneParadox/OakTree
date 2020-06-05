7fb4d6aaea7e0ac17c04790915d4ce06cc84c82f
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-04 18:55:49 -0700

    Removed SYNC_SLOT packet

--- src/main/java/io/github/redstoneparadox/oaktree/client/networking/OakTreeClientNetworking.java
+++ src/main/java/io/github/redstoneparadox/oaktree/client/networking/OakTreeClientNetworking.java
@@ -7,16 +7,7 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
 public class OakTreeClientNetworking {
-
-	private static final Identifier SYNC_SLOT = new Identifier("oaktree", "sync_slot");
 	public static final Identifier SYNC_STACK = new Identifier("oaktree", "sync_stack");
-
-	@Deprecated
-	public static void syncSlot(int x, int y, int index, int syncID) {
-		PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-		buf.writeIntArray(new int[]{x, y, index, syncID});
-		ClientSidePacketRegistry.INSTANCE.sendToServer(SYNC_SLOT, buf);
-	}
 
 	public static void syncStack(int slot, int inventoryID, int syncID, ItemStack stack) {
 		PacketByteBuf buffer = new PacketByteBuf(Unpooled.buffer());
--- src/main/java/io/github/redstoneparadox/oaktree/networking/OakTreeNetworking.java
+++ src/main/java/io/github/redstoneparadox/oaktree/networking/OakTreeNetworking.java
@@ -1,36 +1,20 @@
 package io.github.redstoneparadox.oaktree.networking;
 
-import io.github.redstoneparadox.oaktree.mixin.container.SlotAccessor;
 import io.github.redstoneparadox.oaktree.util.InventoryScreenHandler;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
+
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.slot.Slot;
 import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class OakTreeNetworking {
-
-	private static final Identifier SYNC_SLOT = new Identifier("oaktree", "sync_slot");
-	private static final Map<Integer, ScreenHandler> CONTAINER_MAP = new HashMap<>();
-
 	public static final Identifier SYNC_STACK = new Identifier("oaktree", "sync_stack");
 	private static final Map<Integer, InventoryScreenHandler> LISTENER_MAP = new HashMap<>();
 
 	public static void initPackets() {
-		ServerSidePacketRegistry.INSTANCE.register(SYNC_SLOT, ((context, buffer) -> {
-			int[] data = buffer.readIntArray();
-
-			int x = data[0];
-			int y = data[1];
-			int index = data[2];
-			int syncID = data[3];
-
-			syncSlot(x, y, index, syncID);
-		}));
 		ServerSidePacketRegistry.INSTANCE.register(SYNC_STACK, (context, buffer) -> {
 			int[] data = buffer.readIntArray();
 
@@ -44,32 +28,12 @@ public class OakTreeNetworking {
 		});
 	}
 
-	@Deprecated
-	public static void addContainerForSyncing(ScreenHandler screenHandler) {
-		CONTAINER_MAP.put(screenHandler.syncId, screenHandler);
-	}
-
 	public static void listenForStackSync(InventoryScreenHandler handler) {
 		LISTENER_MAP.put(handler.getSyncID(), handler);
 	}
 
 	public static void stopListening(InventoryScreenHandler handler) {
 		LISTENER_MAP.remove(handler.getSyncID());
-	}
-
-	@Deprecated
-	private static void syncSlot(int x, int y, int index, int syncID) {
-		if (CONTAINER_MAP.containsKey(syncID)) {
-			ScreenHandler screenHandler = CONTAINER_MAP.get(syncID);
-			if (index < screenHandler.slots.size()) {
-				Slot slot = screenHandler.slots.get(index);
-				((SlotAccessor) slot).setX(x);
-				((SlotAccessor) slot).setY(y);
-			}
-		}
-		else {
-			System.out.println("Container with syncID `" + syncID + "` is not listening for updates.");
-		}
 	}
 
 	private static void syncStack(int slot, int inventoryID, int syncID, ItemStack stack) {
