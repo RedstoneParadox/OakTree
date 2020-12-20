5cfa661206ef436705e8d336656f84b547bd3c38
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-19 18:26:53 -0800

    Important TODO

--- src/main/java/io/github/redstoneparadox/oaktree/util/ListUtils.java
+++ src/main/java/io/github/redstoneparadox/oaktree/util/ListUtils.java
@@ -3,7 +3,7 @@ package io.github.redstoneparadox.oaktree.util;
 import java.util.List;
 
 public class ListUtils {
-
+	// TODO: Fix this after the ad hoc self-typing fiasco is done with.
 	public static <T extends List> T growList(T list, int targetSize) {
 		while (list.size() < targetSize) {
 			list.add(null);
