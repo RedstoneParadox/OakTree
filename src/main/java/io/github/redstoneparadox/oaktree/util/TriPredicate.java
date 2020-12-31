e3bff64d1a67a5a025c829373b0da07cb8356be2
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-31 15:51:47 -0800

    Deprecated TriPredicate
    All its uses are effectively deprecated; it's supposed to be internal anyways

--- src/main/java/io/github/redstoneparadox/oaktree/util/TriPredicate.java
+++ src/main/java/io/github/redstoneparadox/oaktree/util/TriPredicate.java
@@ -1,5 +1,9 @@
 package io.github.redstoneparadox.oaktree.util;
 
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.ScheduledForRemoval
+@Deprecated
 @FunctionalInterface
 public interface TriPredicate<T, U, V> {
 	boolean test(T t, U u, V v);
