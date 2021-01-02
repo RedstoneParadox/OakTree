6bc24c87d03a37251816b2635c628f15faa7f8cc
RedstoneParadox <spinjitzu00@gmail.com>
2021-01-01 20:15:31 -0800

    TriFunction generic argument names are refactored to follow Java naming convention

--- src/main/java/io/github/redstoneparadox/oaktree/util/TriFunction.java
+++ src/main/java/io/github/redstoneparadox/oaktree/util/TriFunction.java
@@ -1,6 +1,6 @@
 package io.github.redstoneparadox.oaktree.util;
 
 @FunctionalInterface
-public interface TriFunction<T1, T2, T3, R> {
-	R apply(T1 t1, T2 t2, T3 t3);
+public interface TriFunction<T, U, V, R> {
+	R apply(T t, U u, V v);
 }
