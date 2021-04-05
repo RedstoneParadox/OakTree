9faab6b7bb5c19422607f25332fdab33312e64a7
RedstoneParadox <spinjitzu00@gmail.com>
2021-04-05 12:32:39 -0700

    Create ControlTree.java

---
+++ src/main/java/io/github/redstoneparadox/oaktree/control/ControlTree.java
@@ -0,0 +1,13 @@
+package io.github.redstoneparadox.oaktree.control;
+
+public class ControlTree {
+	private final Control root;
+
+	public ControlTree(Control root) {
+		this.root = root;
+	}
+
+	public void render() {
+		
+	}
+}
