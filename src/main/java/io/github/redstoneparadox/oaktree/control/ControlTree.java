027672c0d1d05e04ef5df76a982dc2ea839d4cef
RedstoneParadox <spinjitzu00@gmail.com>
2021-04-05 12:53:00 -0700

    Blocking out ControlTree

--- src/main/java/io/github/redstoneparadox/oaktree/control/ControlTree.java
+++ src/main/java/io/github/redstoneparadox/oaktree/control/ControlTree.java
@@ -1,13 +1,47 @@
 package io.github.redstoneparadox.oaktree.control;
 
+import io.github.redstoneparadox.oaktree.style.ControlStyle;
+import io.github.redstoneparadox.oaktree.style.Theme;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControlTree {
 	private final Control root;
+	private Theme theme = Theme.vanilla();
+	private boolean dirty = true;
+	private List<Control> zIndexedControls = new ArrayList<>();
 
 	public ControlTree(Control root) {
 		this.root = root;
 	}
 
+	public Theme getTheme() {
+		return theme.copy();
+	}
+
+	public void setTheme(Theme theme) {
+		this.theme = theme;
+	}
+
+	public @Nullable ControlStyle getStyle(String styleName) {
+		return theme.get(styleName);
+	}
+
 	public void render() {
-		
+		if (dirty) {
+			zIndexedControls.clear();
+			root.zIndex(zIndexedControls);
+			dirty = false;
+		}
+
+		for (Control control: zIndexedControls) {
+			// prepare
+		}
+
+		for (Control control: zIndexedControls) {
+			// draw
+		}
 	}
 }
