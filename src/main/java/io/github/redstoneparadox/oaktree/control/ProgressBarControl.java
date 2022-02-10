cec7eab9ffc7d81c2f54258f39012c581d0b2d52
RedstoneParadox <spinjitzu00@gmail.com>
2022-02-10 12:26:43 -0800

    ProgressBarControl overrides the new draw method

--- src/main/java/io/github/redstoneparadox/oaktree/control/ProgressBarControl.java
+++ src/main/java/io/github/redstoneparadox/oaktree/control/ProgressBarControl.java
@@ -10,6 +10,8 @@ import net.minecraft.client.util.math.MatrixStack;
  * A node representing a percent-based progress bar.
  */
 public class ProgressBarControl extends Control {
+	public static PainterKey BAR = new PainterKey();
+
 	protected float percent = 100.0f;
 	protected int barWidth = 1;
 	protected int barHeight = 1;
@@ -65,6 +67,28 @@ public class ProgressBarControl extends Control {
 
 	public Direction2D getDirection() {
 		return direction;
+	}
+
+	@Override
+	protected void draw(MatrixStack matrices) {
+		super.draw(matrices);
+
+		int barX = trueX + ((area.getWidth() /2) - (barWidth/2));
+		int barY = trueY + ((area.getHeight() /2) - (barHeight/2));
+		Painter barPainter = theme.get(id, BAR);
+
+		switch (direction) {
+			case UP -> {
+				int drawHeight = (int) (barHeight * (this.percent / 100.0f));
+				barPainter.draw(matrices, barX, barY + (barHeight - drawHeight), barWidth, drawHeight);
+			}
+			case DOWN -> barPainter.draw(matrices, barX, barY, barWidth, (int) (barHeight * (this.percent / 100.0f)));
+			case LEFT -> {
+				int drawWidth = (int) (barWidth * (this.percent / 100.0f));
+				barPainter.draw(matrices, barX - drawWidth, barY, drawWidth, barHeight);
+			}
+			case RIGHT -> barPainter.draw(matrices, barX, barY, (int) (barWidth * (this.percent / 100.0f)), barHeight);
+		}
 	}
 
 	@Override
