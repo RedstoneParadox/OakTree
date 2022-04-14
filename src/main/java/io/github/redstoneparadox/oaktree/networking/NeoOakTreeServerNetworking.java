697dbada9118105858004ece8b01fb84d48b7196
RedstoneParadox <spinjitzu00@gmail.com>
2022-04-14 11:47:07 -0700

    Groundwork for new syncronized inventory

---
+++ src/main/java/io/github/redstoneparadox/oaktree/networking/NeoOakTreeServerNetworking.java
@@ -0,0 +1,36 @@
+package io.github.redstoneparadox.oaktree.networking;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NeoOakTreeServerNetworking {
+	private static final Int2ObjectArrayMap<List<SynchronizedInventory>> INVENTORIES = new Int2ObjectArrayMap<>();
+
+	public static int getNextInventoryID(int syncID) {
+		var list = get(syncID);
+
+		return list.size();
+	}
+
+	@ApiStatus.Internal
+	public static void addInventory(int syncID, SynchronizedInventory inventory) {
+		var list = get(syncID);
+		list.add(inventory);
+	}
+
+	@ApiStatus.Internal
+	public static void removeInventories(int syncID) {
+		if (INVENTORIES.containsKey(syncID)) INVENTORIES.remove(syncID);
+	}
+
+	private static List<SynchronizedInventory> get(int syncID) {
+		if (!INVENTORIES.containsKey(syncID)) {
+			INVENTORIES.put(syncID, new ArrayList<>());
+		}
+
+		return INVENTORIES.get(syncID);
+	}
+}
--- src/main/java/io/github/redstoneparadox/oaktree/networking/SynchronizedInventory.java
+++ src/main/java/io/github/redstoneparadox/oaktree/networking/SynchronizedInventory.java
@@ -1,58 +1,95 @@
 package io.github.redstoneparadox.oaktree.networking;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
 
-public class SynchronizedInventory implements Inventory {
+import java.util.List;
+
+public final class SynchronizedInventory implements Inventory {
+	private final int syncID;
+	private final int inventoryID;
 	private final Inventory synced;
+	private final PlayerEntity player;
 
-	private SynchronizedInventory(Inventory synced) {
+	private SynchronizedInventory(int syncID, int inventoryID, Inventory synced, PlayerEntity player) {
+		this.syncID = syncID;
+		this.inventoryID = inventoryID;
 		this.synced = synced;
+		this.player = player;
+	}
+
+	/**
+	 * Creates a new {@link SynchronizedInventory}
+	 *
+	 * @param syncID The sync id of your {@link ScreenHandler}
+	 * @param synced The {@link Inventory} to sync
+	 * @param player The player using this inventory
+	 * @return A new {@link SynchronizedInventory}
+	 */
+	public static SynchronizedInventory create(int syncID, Inventory synced, PlayerEntity player) {
+		SynchronizedInventory inventory;
+
+		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+			int inventoryID = NeoOakTreeClientNetworking.getNextInventoryID(syncID);
+			inventory = new SynchronizedInventory(syncID, inventoryID, synced, player);
+
+			NeoOakTreeClientNetworking.addInventory(syncID, inventory);
+		} else {
+			int inventoryID = NeoOakTreeServerNetworking.getNextInventoryID(syncID);
+			inventory = new SynchronizedInventory(syncID, inventoryID, synced, player);
+
+			NeoOakTreeServerNetworking.addInventory(syncID, inventory);
+		}
+
+		return inventory;
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return synced.size();
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return synced.isEmpty();
 	}
 
 	@Override
 	public ItemStack getStack(int i) {
-		return null;
+		return synced.getStack(i);
 	}
 
 	@Override
 	public ItemStack removeStack(int i, int j) {
-		return null;
+		return synced.removeStack(i, j);
 	}
 
 	@Override
 	public ItemStack removeStack(int i) {
-		return null;
+		return synced.removeStack(i);
 	}
 
 	@Override
 	public void setStack(int i, ItemStack itemStack) {
-
+		synced.setStack(i, itemStack);
 	}
 
 	@Override
 	public void markDirty() {
-
+		synced.markDirty();
 	}
 
 	@Override
 	public boolean canPlayerUse(PlayerEntity playerEntity) {
-		return false;
+		return true;
 	}
 
 	@Override
 	public void clear() {
-
+		synced.clear();
 	}
 }
