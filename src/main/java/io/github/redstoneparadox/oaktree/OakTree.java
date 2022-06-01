9e1f0a999859c3cc3786d54639bc40f0b11a5f1b
RedstoneParadox <spinjitzu00@gmail.com>
2022-06-01 16:18:47 -0700

    packet ids for new packets

--- src/main/java/io/github/redstoneparadox/oaktree/OakTree.java
+++ src/main/java/io/github/redstoneparadox/oaktree/OakTree.java
@@ -4,6 +4,8 @@ import io.github.redstoneparadox.oaktree.networking.OakTreeServerNetworking;
 import net.fabricmc.api.ModInitializer;
 
 public class OakTree implements ModInitializer {
+	public static String MODID = "oaktree";
+
 	@Override
 	public void onInitialize() {
 		System.out.println("Loading Oak Tree GUI Toolkit...");
--- src/main/java/io/github/redstoneparadox/oaktree/networking/PacketIdentifiers.java
+++ src/main/java/io/github/redstoneparadox/oaktree/networking/PacketIdentifiers.java
@@ -1,11 +1,22 @@
 package io.github.redstoneparadox.oaktree.networking;
 
+import io.github.redstoneparadox.oaktree.OakTree;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
 public class PacketIdentifiers {
-	public static final Identifier RECEIVE_TRANSFER_RESULT = new Identifier("oaktree", "sync_stack");
-	public static final Identifier UPDATE_STACKS = new Identifier("oaktree", "update_stacks");
+	public static final Identifier UPDATE_STACKS = id("update_stacks");
+	@Deprecated
 	public static final Identifier TRANSFER_STACK = new Identifier("oaktree","transfer_stack");
+
+	public static final Identifier PICKUP_STACK = id("pickup_stack");
+	public static final Identifier PLACE_STACK = id("place_stack");
+	public static final Identifier MOVE_TO_HOTBAR = id("move_to_hotbar"); // used for offhand
+	public static final Identifier SWITCH_INVENTORIES = id("switch_inventories");
+	public static final Identifier PICK_ITEM = id("pick_item"); // creative mode
+
+	static Identifier id(String name) {
+		return new Identifier(OakTree.MODID, name);
+	}
 }
