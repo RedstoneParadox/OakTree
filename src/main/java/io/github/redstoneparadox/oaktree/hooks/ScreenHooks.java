4bc7e52346aa132a2fd3a95ad39bf122c33601b3
RedstoneParadox <spinjitzu00@gmail.com>
2022-06-03 22:28:25 -0700

    scheduled screenhooks for removal

--- src/main/java/io/github/redstoneparadox/oaktree/hooks/ScreenHooks.java
+++ src/main/java/io/github/redstoneparadox/oaktree/hooks/ScreenHooks.java
@@ -2,9 +2,12 @@ package io.github.redstoneparadox.oaktree.hooks;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.screen.ScreenHandler;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Optional;
 
+@Deprecated
+@ApiStatus.ScheduledForRemoval
 public interface ScreenHooks {
 	Optional<ScreenHandler> getHandler();
 
