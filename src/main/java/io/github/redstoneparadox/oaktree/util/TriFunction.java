45ba4cfdea8f036bf900a80f176ca1da4afcca51
RedstoneParadox <spinjitzu00@gmail.com>
2023-07-22 18:39:53 -0700

    Update TriFunction to be consistent with BiFunction

--- src/main/java/io/github/redstoneparadox/oaktree/util/TriFunction.java
+++ src/main/java/io/github/redstoneparadox/oaktree/util/TriFunction.java
@@ -1,6 +1,53 @@
 package io.github.redstoneparadox.oaktree.util;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * Represents a function that accepts two arguments and produces a result.
+ * This is the two-arity specialization of {@link Function}.
+ *
+ * <p>This is a functional
+ * whose functional method is {@link #apply(Object, Object, Object)}.
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <V> the type of the third agument to the function
+ * @param <R> the type of the result of the function
+ *
+ * @see Function
+ */
 @FunctionalInterface
 public interface TriFunction<T, U, V, R> {
+	/**
+	 * Applies this function to the given arguments.
+	 *
+	 * @param t the first function argument
+	 * @param u the second function argument
+	 * @param v the third function argument
+	 * @return the function result
+	 */
 	R apply(T t, U u, V v);
+
+	/**
+	 * Returns a composed function that first applies this function to
+	 * its input, and then applies the {@code after} function to the result.
+	 * If evaluation of either function throws an exception, it is relayed to
+	 * the caller of the composed function.
+	 *
+	 * @param <W> the type of output of the {@code after} function, and of the
+	 *           composed function
+	 * @param after the function to apply after this function is applied
+	 * @return a composed function that first applies this function and then
+	 * applies the {@code after} function
+	 * @throws NullPointerException if after is null
+	 */
+	@Contract(pure = true)
+	default <W> TriFunction<T, U, V, W> andThen(@NotNull Function<R, W> after) {
+		Objects.requireNonNull(after);
+		return  (t, u, v) -> after.apply(apply(t, u, v));
+	}
 }
